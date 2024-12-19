399fe29b5153b2c3610ab2e37b7c7a4541b60be2
DeVMadness <vavrycovychostap@gmail.com>
2024-12-19 15:57:48 +0200

    Implemented DAL

---
+++ ToDoListAppApi/DAL/Context/ToDoListContext.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Context
+{
+    public class ToDoListContext : DbContext
+    {
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        public ToDoListContext()
+            : base(new DbContextOptionsBuilder<ToDoListContext>()
+                  .UseSqlServer(@Environment.GetEnvironmentVariable("ToDoListDbConnectionString")).UseLazyLoadingProxies().Options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public ToDoListContext(DbContextOptions<ToDoListContext> options)
+         : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+
+}
---
+++ ToDoListAppApi/DAL/Domain/Assignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Domain
+{
+    public class Assignment : BaseEntity
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int StatusId { get; set; }
+
+        [ForeignKey(nameof(StatusId))]
+        public virtual Status? Status { get; set; }
+        public virtual DateTime CreatedAt { get; set; }
+    }
+}
