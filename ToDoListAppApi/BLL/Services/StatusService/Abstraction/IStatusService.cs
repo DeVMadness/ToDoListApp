9c9774dbcc93ee1607ec662c1eceb72631ab3f91
DeVMadness <105108377+DeVMadness@users.noreply.github.com>
2025-01-09 17:43:10 +0100

    implemented logic with status (#5)
    * implemented logic with status
    
    * adjusted the put method

---
+++ ToDoListAppApi/BLL/Services/StatusService/Abstraction/IStatusService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services.StatusService.Abstraction
+{
+    public interface IStatusService
+    {
+        Task<List<Status>> GetAllStatusAsync();
+
+    }
+}
---
+++ ToDoListAppApi/ToDoListAppApi/Controllers/StatusController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BLL.Services.StatusService.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : Controller
+    {
+        private readonly IStatusService statusService;
+        private readonly IMapper mapper;
+        private readonly ILogger<StatusController> logger;
+
+        public StatusController(IStatusService statusService, IMapper mapper, ILogger<StatusController> logger)
+        {
+            this.statusService = statusService;
+            this.mapper = mapper;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<StatusResponse>>> GetAllStatuses()
+        {
+            var statuses = await statusService.GetAllStatusAsync();
+
+            return Ok(mapper.Map<List<StatusResponse>>(statuses));
+        }
+    }
+}
